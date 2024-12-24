2261639d99e278e2218d95e6983ad76f31224fd6
Tomas Franzén <tomas@tomasf.se>
2024-12-24 19:32:34 +0100

    Add fill rule

--- Sources/Manifold/CrossSection/CrossSection+Shapes.swift
+++ Sources/Manifold/CrossSection/CrossSection+Shapes.swift
@@ -13,7 +13,41 @@ public extension CrossSection {
         Self(manifold.CrossSection.Circle(radius, Int32(segmentCount)))
     }
 
-    static func polygon(_ polygon: [any Vector2]) -> Self {
-        Self(manifold.CrossSection(.init(polygon.map(\.vec2)), .NonZero))
+    static func polygon(_ polygon: [any Vector2], fillRule: FillRule) -> Self {
+        Self(manifold.CrossSection(.init(polygon.map(\.vec2)), fillRule.primitive))
+    }
+
+    static func polygons(_ polygons: [[any Vector2]], fillRule: FillRule) -> Self {
+        Self(manifold.CrossSection(.init(polygons.map { .init($0.map(\.vec2)) }), fillRule.primitive))
+    }
+}
+
+public extension CrossSection {
+    enum FillRule {
+        case evenOdd
+        case nonZero
+        case positive
+        case negative
+
+        internal init(_ primitive: manifold.CrossSection.FillRule) {
+            switch primitive {
+            case .EvenOdd: self = .evenOdd
+            case .NonZero: self = .nonZero
+            case .Positive: self = .positive
+            case .Negative: self = .negative
+            @unknown default:
+                assertionFailure("Unknown fill rule")
+                self = .nonZero
+            }
+        }
+
+        var primitive: manifold.CrossSection.FillRule {
+            switch self {
+            case .evenOdd: return .EvenOdd
+            case .nonZero: return .NonZero
+            case .positive: return .Positive
+            case .negative: return .Negative
+            }
+        }
     }
 }
